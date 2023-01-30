2e5dd2fb274374e4f34fd05da7576f8f25e5d546
Ross Alex <wacdis.com@gmail.com>
2023-01-30 12:35:25 +0100

    added forms.scss and Text starting template

--- js/Worms/Elements/Text.js
+++ js/Worms/Elements/Text.js
@@ -1,17 +1,11 @@
 import Basics from "../Basics.js";
 
 export default class Text extends Basics {
-	default = {
-		config: {
-			"id": "lt1",
-			"type": "Layout",
-			"data": {
-				"title": "My form layout"
-			},
-			"attrs": {} // Element HTML attributes
-		}
-	};
-	constructor() {
-		super();
+	constructor(config) {
+		super(config);
+	}
+	render() {
+		var nodes = this.dom(``);;
+		return nodes.childNodes;
 	}
 }
